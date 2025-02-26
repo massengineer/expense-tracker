9967468779b5ed55185687eeb28b3e63a38411b1
Louis Massingham <143968432+massengineer@users.noreply.github.com>
2025-02-26 12:34:22 +0000

    Provided some ideas  that need to be done for an improved UI for better UX

--- frontend/src/components/ExpenseForm.jsx
+++ frontend/src/components/ExpenseForm.jsx
@@ -32,9 +32,18 @@ export default function ExpenseForm({ fetchExpenses }) {
       <input
         type="text"
         placeholder="Category"
-        onChange={(e) => setExpense({ ...expense, category: e.target.value })}
+        onChange={(e) => setExpense({ ...expense, category: e.target.value })}  
         className="p-2 mb-2"
       />
+      {/* TODO:
+      <input 
+        type="date"
+        placeholder="Date"
+        onChange={(e) => setExpense({ ...expense, date: e.target.value })}  
+        className="p-2 mb-2" 
+      /> This is a preliminary input field but will most likely chamge and not 
+      be like this as I want the date to have a calendar date picker and the category 
+      to have a dropdown menu with the option of selecting other and then adding custom category*/} 
       <button
         type="submit"
         className="p-2 bg-purple-600 text-white rounded-r-md"
