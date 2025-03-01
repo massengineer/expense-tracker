cb8bcd70efa47707cd2986732d6a0684e40c8de3
Louis Massingham <louisjohnmassingham@gmail.com>
2025-03-01 23:06:57 +0000

    added date to the x-axis on the expense graph

--- frontend/src/components/ExpenseCharts.jsx
+++ frontend/src/components/ExpenseCharts.jsx
@@ -68,7 +68,7 @@ export default function ExpenseCharts({ expenses }) {
         <ResponsiveContainer width="100%" height={300}>
           <LineChart data={expenses}>
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="title" />
+            <XAxis dataKey="date" />
             <YAxis />
             <Tooltip />
             <Line
